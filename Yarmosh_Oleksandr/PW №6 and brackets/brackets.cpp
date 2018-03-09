0bff8264ed9a0ae8955c051f43cf8403700d37dc
YarmakOleksndr <kaniv2011@gmail.com>
2018-03-09 18:41:44 +0200

    Added file

--- Yarmosh_Oleksandr/PW №6 and brackets/brackets.cpp
+++ Yarmosh_Oleksandr/PW №6 and brackets/brackets.cpp
@@ -1,4 +1,6 @@
+#include "stdafx.h"
 #include <iostream>
+#include <Windows.h>
 #include <string>
 #include <stack>
 
@@ -6,42 +8,42 @@ using namespace std;
 
 int main()
 {
-   // setlocale(LC_ALL, "Russian");
+ setlocale(LC_ALL, "Russian");
 
     string strokaVvoda;
     cout << "Enter a string: ";
     getline(cin, strokaVvoda);
-    int last = strokaVvoda.length()-1;
+    int last = strokaVvoda.length() - 1;
     stack <char> steck;
 
-    for_each (char c  in strokaVvoda);
+    for each(char c  in strokaVvoda)
     {
         steck.push(c);
     }
-    cout << "Placed in a stack"<<endl;
+    cout << "Placed in a stack" << endl;
     while (!steck.empty())
     {
         if ((steck.top() == ')') || (steck.top() == '('))
-        if ((steck.top() == '}') || (steck.top() == '{'))
-        if ((steck.top() == ']') || (steck.top() == '['))
-        {
-            cout << "Bracket \"" << steck.top() << "\" is present"<< endl;
+            if ((steck.top() == '}') || (steck.top() == '{'))
+                if ((steck.top() == ']') || (steck.top() == '['))
+                {
+                    cout << "Bracket \"" << steck.top() << "\" is present" << endl;
 
-        }
+                }
         steck.pop();
 
     }
- 
+
     cout << "Stack: ";
     while (!steck.empty())
     {
- 
+
         cout << steck.top();
 
         steck.pop();
 
     }
-    cout << strokaVvoda<<endl;
+    cout << strokaVvoda << endl;
     system("pause");
     return 0;
 }
